da4e7ffb12001bc1e8a28cd87566de4c2c0e3c1e
Richard Möhn <richard.moehn@posteo.de>
2018-08-08 08:56:32 +0900

    WHITESPACE test_basic: Remove superfluous empty line
    I must have accidentally added it at some point. Remove it, because it
    is inconsistent with the rest of the file.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -93,7 +93,6 @@ class TestBasic(unittest.TestCase):
     def testUnlockedLockedPointer(self):
         """Test whether root reply with an unlocked and a locked pointer works.
         """
-
         db = Datastore()
         sched = Scheduler(db)
 
